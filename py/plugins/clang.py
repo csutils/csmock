acba2101a704ea6bba9ac8e01a678160152a7b3f
Kamil Dudka <kdudka@redhat.com>
2014-08-27 13:23:16 +0200

    clang: filter out the noisy warnings
    ... saying "Value stored to '...' is never read"

--- py/plugins/clang.py
+++ py/plugins/clang.py
@@ -46,7 +46,8 @@ class Plugin:
         props.enable_cswrap()
         props.env["CSWRAP_TIMEOUT_FOR"] += ":clang:clang++"
         props.cswrap_filters += \
-                ["csgrep --invert-match --checker CLANG_WARNING --event error"]
+                ["csgrep --invert-match --checker CLANG_WARNING --event error",
+                 "csgrep --invert-match --checker CLANG_WARNING --msg \"Value stored to '.*' is never read\""]
 
         props.install_pkgs += ["clang"]
 
