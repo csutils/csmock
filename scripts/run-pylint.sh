3a27ee02214e7a8bb8619e9baa06b1e2dce9625c
Kamil Dudka <kdudka@redhat.com>
2014-09-15 11:59:36 +0200

    pylint: log file names of scripts being scanned

--- scripts/run-pylint.sh
+++ scripts/run-pylint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 find "$@" -type f -print0 \
-    | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.py$" || head -n1 "$i" | grep "^#!.*python"; } >/dev/null && readlink -f "$i"; done' \
+    | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.py$" || head -n1 "$i" | grep "^#!.*python"; } >/dev/null && readlink -f "$i" | tee /dev/fd/2; done' \
     | xargs pylint -rn --msg-template '{abspath}:{line}:{column}: {msg_id}[pylint]: {msg}'
