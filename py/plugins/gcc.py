0903dfc806de568ea230efa04434abf0f76334b1
Kamil Dudka <kdudka@redhat.com>
2015-04-08 18:48:18 +0200

    gcc: introduce options to add custom compiler flags

--- py/plugins/gcc.py
+++ py/plugins/gcc.py
@@ -43,10 +43,35 @@ class Plugin:
 -w1 appends -Wall and -Wextra, and -w2 enables some other useful warnings. \
 (automatically enables the GCC plug-in)")
 
+        parser.add_argument("--gcc-add-flag", action="append", default=[],
+                help="append the given compiler flag when invoking gcc \
+(can be used multiple times)")
+
+        parser.add_argument("--gcc-add-c-only-flag", action="append", default=[],
+                help="append the given compiler flag when invoking gcc for C \
+(can be used multiple times)")
+
+        parser.add_argument("--gcc-add-cxx-only-flag", action="append", default=[],
+                help="append the given compiler flag when invoking gcc for C++ \
+(can be used multiple times)")
+
     def handle_args(self, parser, args, props):
         if args.gcc_warning_level is not None:
             self.enable()
             self.flags |= flags_by_warning_level(args.gcc_warning_level)
+
+        # serialize custom compiler flags
+        add_cflags = ""
+        add_cxxflags = ""
+        for flag in args.gcc_add_flag:
+            add_cflags += ":" + flag
+            add_cxxflags += ":" + flag
+        for flag in args.gcc_add_c_only_flag:
+            add_cflags += ":" + flag
+        for flag in args.gcc_add_cxx_only_flag:
+            add_cxxflags += ":" + flag
+        if 0 < len(add_cflags) or 0 < len(add_cxxflags):
+            self.enable()
 
         if not self.enabled:
             # drop COMPILER_WARNING defects mistakenly enabled by other plug-ins
@@ -58,6 +83,9 @@ class Plugin:
         props.cswrap_filters += \
             ["csgrep --invert-match --checker COMPILER_WARNING --event error"]
 
+        # write all compiler flags to the environment
         self.flags.write_to_env(props.env)
+        props.env["CSWRAP_ADD_CFLAGS"] += add_cflags
+        props.env["CSWRAP_ADD_CXXFLAGS"] += add_cxxflags
 
         csmock.common.util.install_default_toolver_hook(props, "gcc")
