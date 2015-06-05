b67f71516ab6f8f1a0676a8e24022eb7077aa61a
Kamil Dudka <kdudka@redhat.com>
2015-06-05 15:33:48 +0200

    run-{pylint,shellcheck}.sh: do nothing with no input files
    ... and output the list of defects to standard error output, too

--- scripts/run-shellcheck.sh
+++ scripts/run-shellcheck.sh
@@ -2,5 +2,6 @@
 
 find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.sh$" || head -n1 "$i" | grep "^#!.*sh$"; } >/dev/null && readlink -f "$i" | tee /dev/fd/2; done' \
-    | xargs shellcheck --format=gcc \
-    | sed 's/$/ <--[shellcheck]/'
+    | xargs -r shellcheck --format=gcc \
+    | sed 's/$/ <--[shellcheck]/' \
+    | tee /dev/fd/2
