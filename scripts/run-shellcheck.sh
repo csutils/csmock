d09d24fcbd83a890b0e90d8b367aef3103addf5b
Kamil Dudka <kdudka@redhat.com>
2015-12-21 16:45:12 +0100

    run-shellcheck.sh: sort file list before scanning
    ... to provide comparable results when using multiple workers

--- scripts/run-shellcheck.sh
+++ scripts/run-shellcheck.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+export LC_ALL="C"
 
 find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.sh\$" \
         || head -n1 "$i" | grep -Ev "(tcl|wi)sh\$" | grep "^#!.*sh\$"
-        } >/dev/null && readlink -f "$i" | tee /dev/fd/2; done' \
+        } >/dev/null && readlink -f "$i"; done' \
+    | sort -V | tee /dev/fd/2 \
     | xargs -r shellcheck --format=gcc \
     | sed 's/$/ <--[shellcheck]/' \
     | tee /dev/fd/2
