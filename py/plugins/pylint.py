95e8053a17cfe5b9e4ff827157f456f06ab3b90f
Kamil Dudka <kdudka@redhat.com>
2016-06-10 16:03:00 +0200

    pylint: make the event filter customizable
    Suggested-by: Tomas Tomecek

--- py/plugins/pylint.py
+++ py/plugins/pylint.py
@@ -22,7 +22,7 @@ run_pylint_sh = "/usr/share/csmock/scripts/run-pylint.sh"
 pylint_capture = "/builddir/pylint-capture.err"
 
 filter_cmd = "csgrep --quiet '%s' " \
-        "| csgrep --event '^W[0-9]+' " \
+        "| csgrep --event '%s' " \
         "> '%s'"
 
 class PluginProps:
@@ -41,6 +41,9 @@ class Plugin:
 
     def init_parser(self, parser):
         csmock.common.util.install_script_scan_opts(parser, "pylint")
+        parser.add_argument("--pylint-evt-filter", default="^W[0-9]+",
+                help="filter out Pylint defects whose key event matches the given regex \
+(defaults to '^W[0-9]+', use '.*' to get all defects detected by Pylint)")
 
     def handle_args(self, parser, args, props):
         if not self.enabled:
@@ -61,7 +64,7 @@ class Plugin:
         def filter_hook(results):
             src = results.dbgdir_raw + pylint_capture
             dst = "%s/pylint-capture.err" % results.dbgdir_uni
-            cmd = filter_cmd % (src, dst)
+            cmd = filter_cmd % (src, args.pylint_evt_filter, dst)
             return results.exec_cmd(cmd, shell=True)
 
         props.post_process_hooks += [filter_hook]
