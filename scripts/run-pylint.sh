fecbeff56d48cc4436216d290b131026ec040cef
Kamil Dudka <kdudka@redhat.com>
2017-09-25 08:55:48 +0200

    run-{pylint,shellcheck}.sh: fix off-by-one error
    ... in an explicit invocation of shell

--- scripts/run-pylint.sh
+++ scripts/run-pylint.sh
@@ -4,7 +4,7 @@ export LC_ALL="C"
 find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.py\$" \
         || head -n1 "$i" | grep "^#!.*python"
-        } >/dev/null && readlink -f "$i"; done' \
+        } >/dev/null && readlink -f "$i"; done' "$0" \
     | sort -V | tee /dev/fd/2 \
     | xargs -r pylint -rn --msg-template '{abspath}:{line}:{column}: {msg_id}[pylint]: {msg}' \
     | tee /dev/fd/2
--- scripts/run-shellcheck.sh
+++ scripts/run-shellcheck.sh
@@ -4,7 +4,7 @@ export LC_ALL="C"
 find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.sh\$" \
         || head -n1 "$i" | grep -Ev "(tcl|wi)sh\$" | grep "^#!.*sh\$"
-        } >/dev/null && readlink -f "$i"; done' \
+        } >/dev/null && readlink -f "$i"; done' "$0" \
     | sort -V | tee /dev/fd/2 \
     | xargs -r shellcheck --format=gcc \
     | sed 's/$/ <--[shellcheck]/' \
