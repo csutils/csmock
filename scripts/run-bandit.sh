ed19274ef098651b6e100c4fc521c9fd32c6bf55
Kamil Dudka <kdudka@redhat.com>
2018-02-07 14:04:53 +0100

    scripts/run-bandit.sh: do not assign array to string
    Error: SHELLCHECK_WARNING:
    /usr/share/csmock/scripts/run-bandit.sh:6:13: warning: Assigning an array
    to a string! Assign as array, or use * instead of @ to concatenate. [SC2124]
    4|   # $1 positional argument is security level definition
    5|   SEC_LEVEL=$1
    6|-> TARGET_PATH=${@:2:$#}
    7|
    8|   # Debug

--- scripts/run-bandit.sh
+++ scripts/run-bandit.sh
@@ -3,22 +3,22 @@ export LC_ALL="C"
 
 # $1 positional argument is security level definition
 SEC_LEVEL=$1
-TARGET_PATH=${@:2:$#}
+shift
 
 # Debug
 echo "[DEBUG]	SEC_LEVEL = $SEC_LEVEL"
-echo "[DEBUG]	TARGET_PATH = $TARGET_PATH"
+echo "[DEBUG]	TARGET_PATH = $*"
 
 if [[ -z "${SEC_LEVEL// }" ]]; then
 	echo "[ERROR]	SEC_LEVEL parameter is empty" >&2
 	exit 1
-elif [[ -z "${TARGET_PATH// }" ]]; then
+elif [[ -z "$*" ]]; then
 	echo "[ERROR]	TARGET_PATH parameter is empty" >&2
 	exit 1
 fi
 
 # Find all python files and run bandit analysis 
-find $TARGET_PATH -type f -print0 \
+find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.py\$" \
         || head -n1 "$i" | grep "^#!.*python"
         } >/dev/null && readlink -f "$i"; done' "$0" \
