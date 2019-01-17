2ce5a32302a9a9fb17f3f1b54acc7210964c1f77
Kamil Dudka <kdudka@redhat.com>
2019-01-17 15:41:46 +0100

    run-shellcheck.sh: exclude /usr/share/dirsrv/script-templates/*
    ... in order to eliminate 178 noisy warnings on legacy code installed
    by 389-ds-base-legacy-tools-1.4.0.20-5.module+el8+2685+6de0feb1

--- scripts/run-shellcheck.sh
+++ scripts/run-shellcheck.sh
@@ -5,6 +5,7 @@ find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.sh\$" \
         || head -n1 "$i" | grep -Ev "(tcl|wi)sh\$" | grep "^#!.*sh\$"
         } >/dev/null && readlink -f "$i"; done' "$0" \
+    | grep -v '^/builddir/build/BUILDROOT/[^/]\+/usr/share/dirsrv/script-templates/' \
     | sort -V | tee /dev/fd/2 \
     | xargs -r shellcheck --format=gcc \
     | sed 's/$/ <--[shellcheck]/' \
