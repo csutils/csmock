1240ca6a096cbbabe4420b9f5c6c09c42dc3adb7
Kamil Dudka <kdudka@redhat.com>
2019-09-12 14:40:15 +0200

    chroot-fixups/rpm-build-scripts.sh: kill brp-strip-static-archive
    It is not needed for csmock and breaks scan of gcc-toolset-9-gcc-9.1.1-2.3.el8.

--- scripts/chroot-fixups/rpm-build-scripts.sh
+++ scripts/chroot-fixups/rpm-build-scripts.sh
@@ -4,3 +4,8 @@ script="/usr/lib/rpm/redhat/brp-mangle-shebangs"
 if [ -x $script ]; then
     (set -x; sed -e 's/fail=1/fail=0/' -i $script )
 fi
+
+script="/usr/lib/rpm/brp-strip-static-archive"
+if [ -f $script ]; then
+    ln -sfvT /bin/true $script
+fi
