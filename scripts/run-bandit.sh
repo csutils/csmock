bca95162e66756cc73ebcdfbce3cb08bdabbbcbe
Kamil Dudka <kdudka@redhat.com>
2021-03-04 11:34:02 +0100

    scripts/run-*.sh: use `grep --text` to match shebang
    ... to eliminate the following noise in scan.log:
    
    >>> 2021-03-04 11:21:42 "/usr/bin/mock-unbuffered" "-r" "rhel-9-alpha-x86_64" "--plugin-option=tmpfs:keep_mounted=True" "--config-opts=print_main_output=True" "--quiet" "--chroot" "/usr/share/csmock/scripts/run-shellcheck.sh /builddir/build/BUILDROOT > /builddir/shellcheck-capture.err"
    grep: (standard input): binary file matches
    grep: (standard input): binary file matches
    grep: (standard input): binary file matches
    [...]

--- scripts/run-bandit.sh
+++ scripts/run-bandit.sh
@@ -20,7 +20,7 @@ fi
 # Find all python files and run bandit analysis 
 find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.py\$" \
-        || head -n1 "$i" | grep "^#!.*python"
+        || head -n1 "$i" | grep --text "^#!.*python"
         } >/dev/null && readlink -f "$i"; done' "$0" \
     | sort -V | tee /dev/fd/2 \
     | xargs -r bandit -n -1 -r $SEC_LEVEL --format custom \
--- scripts/run-shellcheck.sh
+++ scripts/run-shellcheck.sh
@@ -3,7 +3,7 @@ export LC_ALL="C"
 
 find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.sh\$" \
-        || head -n1 "$i" | grep -Ev "(atf-|tcl|wi)sh\$" | grep "^#!.*sh\$"
+        || head -n1 "$i" | grep --text -Ev "(atf-|tcl|wi)sh\$" | grep --text "^#!.*sh\$"
         } >/dev/null && readlink -f "$i"; done' "$0" \
     | grep -v '^/builddir/build/BUILDROOT/[^/]\+/usr/share/dirsrv/script-templates/' \
     | sort -V | tee /dev/fd/2 \
