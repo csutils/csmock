1c9a56eb5e9bfb48158d244dc027b15da8d88fbd
Kamil Dudka <kdudka@redhat.com>
2021-12-02 15:55:02 +0100

    symbiotic: patch symbiotic to use /usr/bin/timeout
    Make sure that symbiotic uses the system-provided timeout(1) for itself.
    Otherwise, when formally verifying the coreutils RPM package by Symbiotic,
    the test-suite puts our instrumented binary of timeout(1) into ${PATH},
    causing it to recursively invoke whole symbiotic on each invocation of
    timeout within symbiotic.
    
    Closes: https://github.com/csutils/csmock/pull/54

---
+++ scripts/chroot-fixups/symbiotic-timeout.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+for file in /opt/symbiotic/lib/symbioticpy/symbiotic/{transform,verifier}.py; do
+    [ -w ${file} ] || continue
+
+    # Make sure that symbiotic uses the system-provided timeout(1) for itself.
+    # Otherwise, when formally verifying the coreutils RPM package by Symbiotic,
+    # the test-suite puts our instrumented binary of timeout(1) into ${PATH},
+    # causing it to recursively invoke whole symbiotic on each invocation of
+    # timeout within symbiotic.
+    (
+        set -x
+        sed -e "s|'timeout'|'/usr/bin/timeout'|" -i ${file}
+    )
+done
