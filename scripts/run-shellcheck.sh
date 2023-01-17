47953a416d37774fa8f4dd11a78101c211d5e2ed
Jan Macku <jamacku@redhat.com>
2023-01-17 10:22:02 +0100

    `run-shellcheck`: improve shell script detection
    Closes: https://github.com/csutils/csmock/pull/85

--- scripts/run-shellcheck.sh
+++ scripts/run-shellcheck.sh
@@ -2,8 +2,8 @@
 export LC_ALL="C"
 
 find "$@" -type f -print0 \
-    | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.sh\$" \
-        || head -n1 "$i" | grep --text -Ev "(atf-|tcl|wi)sh\$" | grep --text "^#!.*sh\$"
+    | xargs -0 sh -c 'for i in "$@"; do { printf "%s\\n" "$i" | grep -E "^.*\\.(ash|bash|bats|dash|ksh|sh)$" \
+        || head -n1 "$i" | grep --text -E "^\\s*((\\#|\\!)|(\\#\\s*\\!)|(\\!\\s*\\#))\\s*(\\/usr(\\/local)?)?\\/bin\\/(env\\s+)?(ash|bash|bats|dash|ksh|sh)\\b"
         } >/dev/null && readlink -f "$i"; done' "$0" \
     | grep -v '^/builddir/build/BUILDROOT/[^/]\+/usr/share/dirsrv/script-templates/' \
     | grep -Ev '^/builddir/build/BUILDROOT/[^/]+/usr/share/systemtap/(examples|testsuite)/' \
