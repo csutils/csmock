e971dcdb76d7496bbb097ad10687aa7fb80af859
Kamil Dudka <kdudka@redhat.com>
2023-01-20 14:44:04 +0100

    run-shellcheck.sh: filter shell scripts by a shell function
    The inline shell script became unmaintainable.  No changes in behavior
    intended with this commit.
    
    Closes: https://github.com/csutils/csmock/pull/86

--- scripts/run-shellcheck.sh
+++ scripts/run-shellcheck.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 export LC_ALL="C"
 
+# implementation of the script that filters shell scripts
+filter_shell_scripts() {
+    for i in "$@"; do
+        {
+            printf "%s\\n" "$i" | grep -E "^.*\\.(ash|bash|bats|dash|ksh|sh)$" \
+                || head -n1 "$i" | grep --text -E "^\\s*((\\#|\\!)|(\\#\\s*\\!)|(\\!\\s*\\#))\\s*(\\/usr(\\/local)?)?\\/bin\\/(env\\s+)?(ash|bash|bats|dash|ksh|sh)\\b"
+        } >/dev/null && readlink -f "$i"
+    done
+}
+
+# store a script that filters shell scripts to a variable
+FILTER_SCRIPT="$(declare -f filter_shell_scripts)
+filter_shell_scripts"' "$@"'
+
 find "$@" -type f -print0 \
-    | xargs -0 sh -c 'for i in "$@"; do { printf "%s\\n" "$i" | grep -E "^.*\\.(ash|bash|bats|dash|ksh|sh)$" \
-        || head -n1 "$i" | grep --text -E "^\\s*((\\#|\\!)|(\\#\\s*\\!)|(\\!\\s*\\#))\\s*(\\/usr(\\/local)?)?\\/bin\\/(env\\s+)?(ash|bash|bats|dash|ksh|sh)\\b"
-        } >/dev/null && readlink -f "$i"; done' "$0" \
+    | xargs -0 /bin/bash -c "$FILTER_SCRIPT" "$0" \
     | grep -v '^/builddir/build/BUILDROOT/[^/]\+/usr/share/dirsrv/script-templates/' \
     | grep -Ev '^/builddir/build/BUILDROOT/[^/]+/usr/share/systemtap/(examples|testsuite)/' \
     | sort -V | tee /dev/fd/2 \
