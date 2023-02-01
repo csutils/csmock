2d14611f82101dee8b2061cbc6ba7aac1abe2b6a
Lukáš Zaoral <lzaoral@redhat.com>
2023-02-01 15:24:22 +0100

    run-bandit.sh: fix whitespace errors

--- scripts/run-bandit.sh
+++ scripts/run-bandit.sh
@@ -10,14 +10,14 @@ echo "[DEBUG]	SEC_LEVEL = $SEC_LEVEL" >&2
 echo "[DEBUG]	TARGET_PATH = $*" >&2
 
 if [[ -z "${SEC_LEVEL// }" ]]; then
-	echo "[ERROR]	SEC_LEVEL parameter is empty" >&2
-	exit 1
+    echo "[ERROR]	SEC_LEVEL parameter is empty" >&2
+    exit 1
 elif [[ -z "$*" ]]; then
-	echo "[ERROR]	TARGET_PATH parameter is empty" >&2
-	exit 1
+    echo "[ERROR]	TARGET_PATH parameter is empty" >&2
+    exit 1
 fi
 
-# Find all python files and run bandit analysis 
+# Find all python files and run bandit analysis
 find "$@" -type f -print0 \
     | xargs -0 sh -c 'for i in "$@"; do { printf "%s\n" "$i" | grep "\\.py\$" \
         || head -n1 "$i" | grep --text "^#!.*python"
