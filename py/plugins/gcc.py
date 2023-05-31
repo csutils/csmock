csutils/csmock

commit f7ef6382d280df6ba57926ac3e3c537e8f85ae5e

gcc: disable code fortification with sanitizers
Related: https://github.com/google/sanitizers/issues/247#issuecomment-1283500316

py/plugins/gcc.py
@@ -193,6 +193,15 @@ class Plugin:
             self.flags.append_flags(['-g', '-fno-omit-frame-pointer',
                                      '-fsanitize-recover=all'])
 
+            # sanitizers are not compatible with FORTIFY_SOURCE
+            # https://github.com/google/sanitizers/issues/247#issuecomment-1283500316
+            #
+            # annobin should always be present in the buildroot since it is
+            # required by redhat-rpm-config
+            self.flags.append_flags(["-Wp,-U_FORTIFY_SOURCE",
+                                     "-fplugin=annobin",
+                                     "-fplugin-arg-annobin-no-active-checks"])
+
             # create directory for sanitizer's results
             def create_cap_dir_hook(results, mock):
                 cmd = f"mkdir -pv '{SANITIZER_CAPTURE_DIR}'"

commit 1b018359598d9b2138441323fca0898b1d8c5724

gcc: fix removal of _FORTIFY_SOURCE defines
Fixes the following warnings:
```
Error: COMPILER_WARNING:
rsync-3.2.7/main.c: warning: -D_FORTIFY_SOURCE defined but value is too low

Error: COMPILER_WARNING:
rsync-3.2.7/match.c: warning: -D_FORTIFY_SOURCE defined but value is too low
...
```

Fixes: f7ef6382d280df6ba57926ac3e3c537e8f85ae5e ("gcc: disable code fortification with sanitizers")

py/plugins/gcc.py
@@ -222,14 +222,14 @@ class Plugin:
             self.flags.append_flags(['-g', '-fno-omit-frame-pointer',
                                      '-fsanitize-recover=all'])
 
-            # sanitizers are not compatible with FORTIFY_SOURCE
+            # sanitizers are not compatible with _FORTIFY_SOURCE
             # https://github.com/google/sanitizers/issues/247#issuecomment-1283500316
             #
             # annobin should always be present in the buildroot since it is
             # required by redhat-rpm-config
-            self.flags.append_flags(["-Wp,-U_FORTIFY_SOURCE",
-                                     "-fplugin=annobin",
+            self.flags.append_flags(["-fplugin=annobin",
                                      "-fplugin-arg-annobin-no-active-checks"])
+            self.flags.remove_flags(["-D_FORTIFY_SOURCE=*"])
 
             # create directory for sanitizer's results
             def create_cap_dir_hook(results, mock):