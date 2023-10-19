csutils/csmock

commit e8863429bd61fd5308547b084c14f23ac8432c58

csmock-snyk-plugin: no supported projects fix
Fix: https://issues.redhat.com/browse/OSH-329

Reproducer: -t snyk -r rhel-8-x86_64 --shell-cmd=: <attachee.tar.gz tarball>

Attache needs to be downloaded from https://github.com/RedHatInsights/attache

If no supported project is detected using the snyk plugin, it no longer crashes and no results are parsed

py/plugins/snyk.py
@@ -172,7 +172,9 @@ class Plugin:
             # check exit code of snyk code itself
             if ec == 3:
                 # If there are no supported project, we return no results but no crash.
-                results.print_with_ts("snyk-code: no supported projects detected")
+                results.print_with_ts("snyk-code: no supported project for Snyk")
+                # If no supported project, no results file is generated
+                props.copy_out_files.remove(SNYK_OUTPUT)
                 return 0
             if ec not in [0, 1]:
                 results.error("snyk code returned unexpected exit status: %d" % ec, ec=ec)
@@ -185,6 +187,9 @@ class Plugin:
 
         # convert the results into the csdiff's JSON format
         def filter_hook(results):
+            # If no supported project is found, we don't have results file
+            if not os.path.exists(SNYK_OUTPUT):
+                return 0
             src = results.dbgdir_raw + SNYK_OUTPUT
             dst = "%s/snyk-capture.js" % results.dbgdir_uni
             cmd = FILTER_CMD % (src, dst)

commit e1a73c68f5b8c6f5fca416a0f81fd510180d6084

snyk: do not throw away Snyk results by mistake
... on successful scans.  This commit fixes a major regression
introduced by the previous commit.

Fixes: commit e8863429bd61fd5308547b084c14f23ac8432c58
Related: https://issues.redhat.com/browse/OSH-329
Resolves: https://issues.redhat.com/browse/OSH-362
Closes: https://github.com/csutils/csmock/pull/133

py/plugins/snyk.py
@@ -187,10 +187,10 @@ class Plugin:
 
         # convert the results into the csdiff's JSON format
         def filter_hook(results):
-            # If no supported project is found, we don't have results file
-            if not os.path.exists(SNYK_OUTPUT):
-                return 0
             src = results.dbgdir_raw + SNYK_OUTPUT
+            if not os.path.exists(src):
+                # do not convert SARIF results if they were not provided by Snyk
+                return 0
             dst = "%s/snyk-capture.js" % results.dbgdir_uni
             cmd = FILTER_CMD % (src, dst)
             return results.exec_cmd(cmd, shell=True)