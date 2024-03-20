csutils/csmock

commit 73eddc138c8e9c97246dd2d12ad30a2a13bea3f4

snyk-plugin: added --snyk-code-test-opts flag
Users are now able to use the introduced flag to pass new parameters to the snyk cli

Reproducer: csmock -t snyk --snyk-code-test-opts='--report --project-name=osbuild' -r rhel-8-x86_64 osbuild-99-1.el10+1.src.rpm

Resolves: https://issues.redhat.com/browse/OSH-307
Closes: https://github.com/csutils/csmock/pull/137

py/plugins/snyk.py
@@ -33,7 +33,7 @@ SNYK_LOG = "/builddir/snyk-scan.log"
 FILTER_CMD = f"csgrep '%s' --mode=json --prepend-path-prefix={SNYK_SCAN_DIR}/ > '%s'"
 
 # default value for the maximum amount of time taken by invocation of Snyk (5 hours)
-DEFAULT_SNYK_TIMEOUT=18000
+DEFAULT_SNYK_TIMEOUT = 18000
 
 
 class PluginProps:
@@ -74,6 +74,9 @@ class Plugin:
             "--snyk-timeout", type=int, default=DEFAULT_SNYK_TIMEOUT,
             help="maximum amount of time taken by invocation of Snyk [s]")
 
+        parser.add_argument(
+            "--snyk-code-test-opts",
+            help="space-separated list of additional options passed to the 'snyk code test' command")
 
     def handle_args(self, parser, args, props):
         if not self.enabled:
@@ -103,7 +106,7 @@ class Plugin:
             self.snyk_bin = os.path.join(cache_dir, snyk_bin_name)
 
             if not args.snyk_refresh and os.path.exists(self.snyk_bin):
-                results.print_with_ts("reusing previously downloaded snyk executable: %s" % self.snyk_bin)
+                results.print_with_ts("reusing previously downloaded snyk executable: " + self.snyk_bin)
             else:
                 # fetch the binary executable
                 ec = results.exec_cmd(['curl', '-Lfso', self.snyk_bin, url])
@@ -143,6 +146,7 @@ class Plugin:
         def copy_resolv_conf(results, mock):
             mock.copy_in_resolv_conf()
             return 0
+
         props.post_depinst_hooks += [copy_resolv_conf]
 
         def scan_hook(results, mock, props):
@@ -156,8 +160,13 @@ class Plugin:
                 return ec
 
             # command to run snyk code
-            cmd = "%s code test -d %s --sarif-file-output=%s >/dev/null 2>%s" \
-                    % (self.snyk_bin, SNYK_SCAN_DIR, SNYK_OUTPUT, SNYK_LOG)
+            cmd = f"{self.snyk_bin} code test -d {SNYK_SCAN_DIR}"
+
+            # if we use the --snyk-code-test-opts flags, we append the flags to the SNYK CLI code
+            if args.snyk_code_test_opts:
+                cmd += f" {args.snyk_code_test_opts}"
+
+            cmd += f" --sarif-file-output={SNYK_OUTPUT} >/dev/null 2>{SNYK_LOG}"
 
             if args.snyk_timeout:
                 # wrap snyk invocation by timeout(1)
@@ -194,4 +203,5 @@ class Plugin:
             dst = "%s/snyk-results.json" % results.dbgdir_uni
             cmd = FILTER_CMD % (src, dst)
             return results.exec_cmd(cmd, shell=True)
+
         props.post_process_hooks += [filter_hook]

commit b3503d48696cb2ec8eb2fb379fb57c141f08e8da

snyk: sanitize options passed to --snyk-code-test-opts
... to avoid shell injection.  Also provide `sanitize_opts_arg()`
in `csmock.common.util` so that it can be reused by other plug-ins
that support passing of custom options to the wrapped tools.

Fixes: commit 73eddc138c8e9c97246dd2d12ad30a2a13bea3f4
Resolves: CVE-2024-2243 - command injection vulnerability in csmock-plugin-snyk
Reviewed-by: jperezde <jperezde@redhat.com>

py/common/util.py
@@ -17,6 +17,7 @@
 
 import os
 import re
+import shlex
 
 
 def shell_quote(str_in):
@@ -32,6 +33,30 @@ def shell_quote(str_in):
         else:
             str_out += c
     return "\"" + str_out + "\""
+
+
+def arg_value_by_name(parser, args, arg_name):
+    """return value of an argument parsed by argparse.ArgumentParser"""
+    for action in parser._actions:
+        if arg_name in action.option_strings:
+            return getattr(args, action.dest)
+
+
+def sanitize_opts_arg(parser, args, arg_name):
+    """sanitize command-line options passed to an option of argparse.ArgumentParser"""
+    opts_str = arg_value_by_name(parser, args, arg_name)
+    if opts_str is None:
+        return None
+
+    # split, quote, and rejoin the options to avoid shell injection
+    try:
+        split_opts = shlex.split(args.snyk_code_test_opts)
+
+        # starting with Python 3.8, one can use shlex.join(split_opts)
+        return ' '.join(shlex.quote(arg) for arg in split_opts)
+
+    except ValueError as e:
+        parser.error(f"failed to parse value given to {arg_name}: {str(e)}")
 
 
 def strlist_to_shell_cmd(cmd_in, escape_special=False):

py/plugins/snyk.py
@@ -18,6 +18,7 @@
 import os
 
 from csmock.common.snyk import snyk_write_analysis_meta
+from csmock.common.util import sanitize_opts_arg
 
 
 # default URL to download snyk binary executable
@@ -83,6 +84,9 @@ class Plugin:
     def handle_args(self, parser, args, props):
         if not self.enabled:
             return
+
+        # sanitize options passed to --snyk-code-test-opts to avoid shell injection
+        self.snyk_code_test_opts = sanitize_opts_arg(parser, args, "--snyk-code-test-opts")
 
         # check whether we have access to snyk authentication token
         self.auth_token_src = os.path.expanduser(args.snyk_auth)
@@ -164,9 +168,9 @@ class Plugin:
             # command to run snyk code
             cmd = f"{self.snyk_bin} code test -d {SNYK_SCAN_DIR}"
 
-            # if we use the --snyk-code-test-opts flags, we append the flags to the SNYK CLI code
-            if args.snyk_code_test_opts:
-                cmd += f" {args.snyk_code_test_opts}"
+            if self.snyk_code_test_opts:
+                # propagate options given to --snyk-code-test-opts
+                cmd += f" {self.snyk_code_test_opts}"
 
             cmd += f" --sarif-file-output={SNYK_OUTPUT} >/dev/null 2>{SNYK_LOG}"
 